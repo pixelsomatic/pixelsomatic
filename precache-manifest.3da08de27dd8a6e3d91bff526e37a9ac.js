09921348fb3cb0bca54db0e5d486faf7d634a170
krsnananda <krsnananda@outlook.com>
2020-01-09 02:22:02 -0300

    Updates

---
+++ precache-manifest.3da08de27dd8a6e3d91bff526e37a9ac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90b0cef7958ed924c2091e1a0b7e448",
+    "url": "/krsnananda/index.html"
+  },
+  {
+    "revision": "b9574eab4a5c2e4ad754",
+    "url": "/krsnananda/static/css/main.6636e293.chunk.css"
+  },
+  {
+    "revision": "08bb012c068f4549d1cc",
+    "url": "/krsnananda/static/js/2.f81ac884.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/krsnananda/static/js/2.f81ac884.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9574eab4a5c2e4ad754",
+    "url": "/krsnananda/static/js/main.32a4aedf.chunk.js"
+  },
+  {
+    "revision": "a0997cae80e357954d7e",
+    "url": "/krsnananda/static/js/runtime-main.ec9cd302.js"
+  },
+  {
+    "revision": "bff20d7ac6c077a63a1e374ff65c3754",
+    "url": "/krsnananda/static/media/nerd-logo.bff20d7a.png"
+  },
+  {
+    "revision": "eb7c1f2b2fcc5bd93fcc60394420824a",
+    "url": "/krsnananda/static/media/profile2.eb7c1f2b.jpg"
+  },
+  {
+    "revision": "ecd22f6c8e23fb52a036e88ab8008bb2",
+    "url": "/krsnananda/static/media/share-eat.ecd22f6c.jpg"
+  },
+  {
+    "revision": "3dc2f5c6f6a1a5732b796b15f4e422c5",
+    "url": "/krsnananda/static/media/tic-tac-toe.3dc2f5c6.jpg"
+  },
+  {
+    "revision": "d33cac84865115940e70a576a803dc05",
+    "url": "/krsnananda/static/media/wordpress.d33cac84.png"
+  }
+]);
